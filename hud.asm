25c18e146fc021a3c098c12b5861a93d9f7a2c23
Hernán <pradoh97@gmail.com>
2022-11-13 13:00:19 -0300

    FIX valores del stack

--- hud.asm
+++ hud.asm
@@ -4,30 +4,32 @@
 .data
   titulo            db "N O N O G R A M - U N S A M", 24h
   nivel             db "NIVEL ", 24h
-  nroNivel          db 0 ;es el caracter 23° de la linea de cantidad de errores.
+  numeroNivel       db 0 ;es el caracter 23° de la linea de cantidad de errores.
   errores           db "CANTIDAD DE ERRORRES: ", 24h
-  cantidadErrores   db 0
   cantidadFilas     db 0
   cantidadColumnas  db 0
-  pistaColumna      db ""
-  pistaFila         db ""
+  pistaFila         dw 0
+  pistaColumna      dw 0
   cursorX           db 0
   cursorY           db 0
-  controles         db "Usa W-A-S-D para moverte y ESPACIO para seleccionar. Si deseas volver al menu principal presiona ESC.", 24h
+  controles         db "Usa W-A-S-D para moverte y ESPACIO para seleccionar.", 0dh, 0ah
+                    db " Si deseas volver al menu principal presiona ESC.", 24h
 
 .code
 extrn imprimir:proc
+extrn imprimirCaracter:proc
 extrn cursor:proc
 public hud
-;Recibe por stack:
-; - El número de nivel
-; - Las pistas para las columnas
-; - Las pistas para las filas
-; - El origen de pistas en X
-; - El origen de pistas en Y
-; - La cantidad de filas a imprimir
-; - La cantidad de columnas a imprimir
+public actualizarErrores
 
+;Recibe por stack lo siguiente (lo que está después de los : es el offset para BP):
+; - El número de nivel: 16
+; - Las pistas para las columnas: 14
+; - Las pistas para las filas: 12
+; - El origen de pistas en X: 10
+; - El origen de pistas en Y: 8
+; - La cantidad de filas a imprimir: 10
+; - La cantidad de columnas a imprimir: 8
 hud proc
   ;------------------------------------------------------------------------
   ;ESTABLECEMOS EL CONTENIDO EXTERIOR AL JUEGO:
@@ -39,34 +41,38 @@ hud proc
     mov bp, sp
 
     push dx
-    push cx
     push bx
     push ax
 
   cargarDesdeStack:
-    ;EN TODAS ESTAS, TENIA QUE INDICAR SI ES BYTE O NO?
-    mov al, ss:[bp+4]
-    mov nroNivel, al
+    ;Número de nivel
+    mov dl, ss:[bp+16]
+    mov numeroNivel, dl
 
-    mov al, ss:[bp+6]
-    mov pistaColumna, al
-    mov al, ss:[bp+8]
-    mov pistaFila, al
+    ;Pistas para columnas y después filas
+    mov bx, ss:[bp+14]
+    mov pistaColumna, bx
+    mov bx, ss:[bp+12]
+    mov pistaFila, bx
 
-    mov ax, ss:[bp+10]
-    mov cursorX, al
-    mov ax, ss:[bp+12]
-    mov cursorY, al
+    ;Coordenadas de origen en X y después Y
+    mov dl, ss:[bp+10]
+    mov cursorX, dl
+    mov dl, ss:[bp+8]
+    mov cursorY, dl
 
-    mov al, ss:[bp+14]
-    mov cantidadFilas, al
-    mov al, ss:[bp+16]
-    mov cantidadColumnas, al
+    ;Cantidad de filas y columnas
+    mov dl, ss:[bp+6]
+    mov cantidadFilas, dl
+    mov dl, ss:[bp+4]
+    mov cantidadColumnas, dl
 
   imprimirTituloJuego:
     mov dh, cursorY 		    ;COORDENADA DE FILA
     mov dl, cursorX		      ;COORDENADA DE COLUMNA
+
     call cursor
+
     lea dx, titulo	;IMPPRIMIMOS EL CARTEL DEL TITULO DEL JUEGO
     call imprimir
 
@@ -81,12 +87,10 @@ hud proc
     lea dx, nivel	;IMPPRIMIMOS EL CARTEL DEL NIVEL EN EL QUE ESTA EL JUGADOR
     call imprimir
 
-    add nroNivel, 30h
-    mov ah, 02h
-    mov dl, nroNivel
-    int 21h
-
-    imprimirControles:
+    add numeroNivel, 30h
+    mov dl, numeroNivel
+    call imprimirCaracter
+  imprimirControles:
     mov cursorX, 1
     mov cursorY, 24
 
@@ -99,7 +103,7 @@ hud proc
 
   imprimirErrores:
     mov cursorX, 1
-    mov cursorY, 4
+    mov cursorY, 3
 
     mov dh, cursorY 		    ;COORDENADA DE FILA
     mov dl, cursorX		      ;COORDENADA DE COLUMNA
@@ -109,45 +113,60 @@ hud proc
     call imprimir		    ;LA IDEA ES QUE VAYA INCREMENTANDO Y CUANDO SUPERE, REINICE EL JUEGO
 
     mov ah, 02h
-    add cantidadErrores, 30h
-    mov dl, cantidadErrores
+    mov dl, 30h
     int 21h
 
+  ;Paso previo a imprimir filas.
   mov cl, cantidadFilas
   mov bx, 0
   mov cursorX, 35
   mov cursorY, 10
 
-
   imprimirPistasFilas:
     mov dh, cursorY 		   ;COORDENADA DE FILA
     mov dl, cursorX		     ;COORDENADA DE COLUMNA
     call cursor
-
-    mov dl, pistaFila[bx]	;IMPRIMIMOS LAS PISTAS QUE REFIEREN A LAS FILAS
-    mov ah, 2
-    int 21h
+    mov si, pistaFila
+    mov dl, byte ptr[si+bx]	;IMPRIMIMOS LAS PISTAS QUE REFIEREN A LAS FILAS
+    call imprimirCaracter
     inc bx
     inc cursorY
   loop imprimirPistasFilas
 
-  ; imprimirPistasColumnas:
-  ;   mov cursorX, 37
-  ;   mov cursorY, 9
-  ;
-  ;   mov dh, cursorY 		    ;COORDENADA DE FILA
-  ;   mov dl, cursorX		      ;COORDENADA DE COLUMNA
-  ;   call cursor
-  ;
-  ;   lea dx, pistaColumna	;IMPRIMIMOS LAS PISTAS QUE REFIEREN A LAS COLUMNAS
-  ;   call imprimir
+  imprimirPistasColumnas:
+    mov cursorX, 37
+    mov cursorY, 9
+
+    mov dh, cursorY 		    ;COORDENADA DE FILA
+    mov dl, cursorX		      ;COORDENADA DE COLUMNA
+    call cursor
+
+    mov dx, pistaColumna	;IMPRIMIMOS LAS PISTAS QUE REFIEREN A LAS COLUMNAS
+    call imprimir
 
   restaurarRegistros:
     pop ax
     pop bx
-    pop cx
     pop dx
     pop bp
-ret
+
+  ret 8
 hud endp
+
+;Recibe la cantidad de errores por stack.
+actualizarErrores proc
+  push bp
+  mov bp, sp
+  push bx
+  push dx
+
+  mov dh, 3
+  mov dl, 23
+  call cursor
+
+  mov dl, ss:[bp+4]
+  add dl, 30h
+  call imprimirCaracter
+  ret 6
+actualizarErrores endp
 end
