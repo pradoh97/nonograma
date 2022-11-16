d3acf282c73d64cd9acc0c946deeaed7d20d070d
agusvenanzoni <agusvenanzoni0@gmail.com>
2022-11-16 18:10:28 -0300

    g
    g

--- intruc.asm
+++ intruc.asm
@@ -12,6 +12,7 @@ i2      db  "-La idea del juego es revelar una imagen oculta.", 0dh, 0ah
         db  "tiene que haber al menos un cuadro en blanco entre cada secuencia", 0dh, 0ah
         db  "-El orden tambien es importante. El orden de los cuadros coloreados es el", 0dh, 0ah
         db  "mismo orden en el que aparecen los numeros.", 0dh, 0ah, 24h
+i3			db	"Presione una tecla para regresar al menu principal.", 24h
 
 .code
 
@@ -20,8 +21,35 @@ extrn imprimir:proc
 public intruccion
 intruccion proc
 
+  push ax
+  push dx
 
+      mov dh, 1
+      mov dl, 30
+      call cursor
 
+      lea dx, i1
+      call imprimir
+
+      mov dh, 3
+      mov dl, 1
+      call cursor
+
+      lea dx, i2
+      call imprimir
+
+      mov dh, 24
+      mov dl, 15
+      call cursor
+
+      lea dx, i3
+      call imprimir
+
+      mov ah, 0
+      int 16h
+
+    pop dx
+    pop ax
 
 ret
 intruccion endp
